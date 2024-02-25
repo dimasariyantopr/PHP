fe7f7f8488111edd1dcde90461bb41730f653733
Dimas Ariyanto Prasetyo <149459640+dimasariyantopr@users.noreply.github.com>
2024-02-25 19:40:22 +0700

    Create tambah.php

---
+++ CRUD/tambah.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>Create</title>
+	</head>
+	<body>
+		<h2>CRUD - Create, Read, Update, and Delete</h2>
+		<br>
+		<a href="index.php">Kembali</a>
+		<br>
+		<h3>Tambah data</h3>
+		<form method="post" action="aksi-tambah.php">
+		<table>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>NIS</td>
+				<td><input type="text" name="nis"></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td><input type="text" name="alamat"></td>
+			</tr>
+			<tr>
+			<td></td>
+			<td><input type="submit" value="Tambah data"></td>
+			</tr>
+		</table>
+		</form>
+	</body>
+</html>
