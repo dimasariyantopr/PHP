9bb18c3986cc7e17e02fdc9eb792f2e392306268
Dimas Ariyanto Prasetyo <149459640+dimasariyantopr@users.noreply.github.com>
2024-02-29 17:32:58 +0700

    Create login.php

---
+++ Login_session/login.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	<body>
+		<h1>Login</h1>
+		<form action="aksi-login.php" method="post">
+		<table> 
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td><input type="reset" name="cancel"></td>
+				<td><input type="submit" name="login"></td>
+			</tr>
+		</table>
+		</form>
+	</body>
+</html>
